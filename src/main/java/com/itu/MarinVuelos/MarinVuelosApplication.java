ab6541e75d38307565ea7fd92be4e8d32783b7d8
Cesar Marin <cesarmarinmorla@gmail.com>
2026-05-17 19:16:22 -0300

    chore: primer commit

---
+++ src/main/java/com/itu/MarinVuelos/MarinVuelosApplication.java
@@ -0,0 +1,13 @@
+package com.itu.MarinVuelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarinVuelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarinVuelosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itu/MarinVuelos/MarinVuelosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itu.MarinVuelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarinVuelosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
