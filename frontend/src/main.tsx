88434c376a60aca5dbd8886b30f62a395c992d08
Cesar Marin <cesarmarinmorla@gmail.com>
2026-05-27 23:26:19 -0300

    feat: agregar tema gruvbox dark con tokens CSS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles/gruvbox.css'
 import './index.css'
 import App from './App.tsx'
+
+document.body.classList.add('theme-gruvbox')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
